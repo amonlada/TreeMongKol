731b424e22d46f96df9401fda27a43b368d40839
Thidaratmatha05 <amonlada@kkumail.com>
2021-03-30 17:03:35 +0700

    Treemongkol

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import requests
+import PIL
+from PIL import Image
+import botnoi.resnet50 as rn
+import pickle
+
+app = FastAPI()
+
+# output function
+modFile = 'mymod.mod'
+mod = pickle.load(open(modFile,'rb'))
+
+@app.get("/img")
+async def imgurl(p_image_url: str):
+    #file#Image.open(image.file)
+    r = requests.get(p_image_url, allow_redirects=True)
+    with open("file2.png",'wb') as f:
+        f.write(r.content)
+    feat = rn.extract_feature("file2.png")
+    res = mod.predict([feat])[0]
+    return {"class":str(res)}
+
+if __name__ == '__main__':
+   import uvicorn
+   uvicorn.run(app, host="0.0.0.0", port=8080, debug=True) 
